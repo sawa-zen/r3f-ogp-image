9adcc5be610d5e2733b633ce00bfc7184d0cbd55
sawa-zen <sawadasuiren@gmail.com>
2025-02-09 15:26:17 +0900

    fixup! fixup! fix

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,6 +1,6 @@
 // src/app/api/image/route.ts
 import { NextResponse } from 'next/server';
-import { getBrowser } from '~/lib/browser';
+import puppeteer from 'puppeteer-core';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -8,7 +8,9 @@ export async function GET(request: Request) {
   const scale = parseFloat(searchParams.get('scale') || '1');
 
   // 使い回し中のBrowserを取得
-  const browser = await getBrowser();
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BLESS_TOKEN}&--use-gl=angle&--use-angle=gl`,
+  })
   // 新規ページを開く
   const page = await browser.newPage();
 
@@ -18,17 +20,16 @@ export async function GET(request: Request) {
 
     // 環境に応じてアクセス先URLを決定
     const isDev = process.env.NODE_ENV === 'development';
-    // const siteUrl = isDev
-    //   ? process.env.TUNNEL_URL
-    //   : process.env.NEXT_PUBLIC_SITE_URL || 'https://r3f-ogp-image.vercel.app';
-    const siteUrl = `${process.env.TUNNEL_URL}/test`
+    const siteUrl = isDev
+      ? process.env.TUNNEL_URL
+      : process.env.NEXT_PUBLIC_SITE_URL || 'https://r3f-ogp-image.vercel.app';
 
     // ページを開いて準備ができるまで待機
     await page.goto(`${siteUrl}?text=${text}&scale=${scale}`, {
       timeout: 300000,
       waitUntil: 'networkidle0',
     });
-    // await page.waitForSelector('#ready', { timeout: 300000 });
+    await page.waitForSelector('#ready', { timeout: 300000 });
 
     // スクリーンショット
     const buffer = await page.screenshot({ encoding: 'binary' });
@@ -37,6 +38,7 @@ export async function GET(request: Request) {
     });
   } finally {
     // ページは都度閉じる。Browser自体は閉じない
-    await page.close();
+    await page.close()
+    browser.disconnect()
   }
 }
--- src/components/ThreeCanvas/index.tsx
+++ src/components/ThreeCanvas/index.tsx
@@ -1,7 +1,8 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment } from '@react-three/drei'
 import { TextMesh } from './components/TextMesh'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import opentype from 'opentype.js'
 
 interface Props {
   text: string
@@ -9,43 +10,63 @@ interface Props {
 }
 
 export const ThreeCanvas = ({ text, scale }: Props) => {
-  const [id, setId] = useState<string>("")
+  const [font, setFont] = useState<opentype.Font | undefined>(undefined)
+  const [firstLineReady, setFirstLineReady] = useState(false)
+  const [secondLineReady, setSecondLineReady] = useState(false)
 
-  const handleReady = useCallback(() => {
-    if (id) return
-    setId('ready')
-  }, [id])
+  const handleLoadedFont = useCallback((err: unknown, font: opentype.Font | undefined) => {
+    if (err || !font) {
+      console.error('フォントのロードに失敗しました:', err);
+      return
+    }
+    setFont(font)
+  }, [])
+
+  useEffect(() => {
+    const fontUrl = "./DelaGothicOne-Regular.woff";
+    opentype.load(fontUrl, handleLoadedFont)
+  }, [handleLoadedFont])
 
   return (
     <Canvas
-      id={id}
+      id={firstLineReady && secondLineReady ? "ready" : "loading"}
       className="bg-white"
       style={{ aspectRatio: "1200/630" }}
       camera={{ position: [0, 0, 0] }}
+      frameloop="demand"
     >
       <ambientLight intensity={1} />
       <directionalLight position={[-10, 20, 10]} intensity={1} />
       <directionalLight position={[10, 20, 10]} intensity={1} />
       <Environment preset="studio" />
-      <group
-        position={[0, 0, -3]}
-        rotation={[-Math.PI / 7, 0, 0]}
-        scale={[scale, scale, scale]}
-      >
-        <TextMesh
-          text={text}
-          color="#FF3333"
-          outlineColor="#FFCC00"
-          position={[0, 0.5, 0]}
-          onReady={handleReady}
-        />
-        <TextMesh
-          text="欲しい"
-          color="#FFFFFF"
-          outlineColor="#999999"
-          position={[0, -0.5, 0]}
-        />
-      </group>
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="#FFFF00" />
+      </mesh>
+      {font && (
+        <group
+          position={[0, 0, -3]}
+          rotation={[-Math.PI / 7, 0, 0]}
+          scale={[scale, scale, scale]}
+        >
+          <TextMesh
+            text={text}
+            font={font}
+            color="#FF3333"
+            outlineColor="#FFCC00"
+            position={[0, 0.5, 0]}
+            onReady={() => setFirstLineReady(true)}
+          />
+          <TextMesh
+            text="欲しい"
+            font={font}
+            color="#FFFFFF"
+            outlineColor="#999999"
+            position={[0, -0.5, 0]}
+            onReady={() => setSecondLineReady(true)}
+          />
+        </group>
+      )}
     </Canvas>
   )
 }
