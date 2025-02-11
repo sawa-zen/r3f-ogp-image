9480075e2a470cf22651a3845edb3b4a3114b915
sawa-zen <sawadasuiren@gmail.com>
2025-02-11 11:41:56 +0900

    fixup! fixup! fixup! fixup! fixup! refactor: speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,9 @@
 import "./globals.css";
 
 export async function generateMetadata() {
-  // 環境に応じてアクセス先URLを決定
-  const isDev = process.env.NODE_ENV === 'development';
-  const siteUrl = isDev
-    ? process.env.TUNNEL_URL
-    : process.env.NEXT_PUBLIC_SITE_URL;
-
   return {
     title: "3D 5000兆円欲しいジェネレータ",
     description: "3D版の5000兆円欲しいジェネレータです",
-    // openGraph: {
-    //   images: [{
-    //     url: `${siteUrl}/api/og-image`,
-    //     width: 600,
-    //     height: 315,
-    //   }],
-    // },
-    // twitter: {
-    //   card: 'summary_large_image',
-    //   creator: '@sawa_zen',
-    //   images: [{
-    //     url: `${siteUrl}/api/og-image`,
-    //     width: 600,
-    //     height: 315,
-    //   }],
-    // },
   }
 }
 
